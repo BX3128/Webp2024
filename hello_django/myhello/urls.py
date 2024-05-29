aa633077626ca3573ac9177b0bb259417071b8d8
BX3128 <119848074+BX3128@users.noreply.github.com>
2024-05-29 17:32:23 +0800

    course update 0529

--- hello_django/myhello/urls.py
+++ hello_django/myhello/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.myIndex, name='index'),
+    # path('', views.myIndex, name='index'),
+    # path('', views.HelloApiView.as_view(), name='index'),
+    path('', views.myhello_api, name='index')
 ]
